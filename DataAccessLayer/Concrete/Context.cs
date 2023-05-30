63eb64d69a61d321031fb24b4a2badafb483a935
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2023-05-30 22:51:06 +0300

    DBContext ile bağlantı kuruldu

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    //context classı: veri tabanı yapılandırması bağlantı stringini tutar
+    //veri tabanında bulunacak tabloların isimlerini tutar
+    //Context:DbContext yazmamızın sebebi db setleri kullanabilmektir
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=serveradresigelecekburaya;database=CoreProjeDB ;integrated security=true");
+        }
+
+    }
+}
